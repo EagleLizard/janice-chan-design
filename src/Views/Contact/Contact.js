d7fe475d6e12effa6fdcb1fad7125e256f4e1654
Tyler Whited <whitedtd@gmail.com>
2017-11-27 14:58:10 -0700

    Adjust alignment for contact page

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -13,22 +13,12 @@ export default class Contact extends React.Component {
     return (
       <div className="contact-page">
         <div className="contact-title">
-          contact me
+          Contact
         </div>
         <div className="contact-info">
           <div className="contact-item">
-            <div className="contact-type">
-              Name:
-            </div>
             <div className="contact-content">
-              Janice Chan
-            </div>
-          </div>
-          <div className="contact-item">
-            <div className="contact-type">
-              Email:
-            </div>
-            <div className="contact-content">
+              For inquiries, email me at&nbsp;
               <a href="mailto:contact@janicechan.design">
                 contact@janicechan.design
               </a>
